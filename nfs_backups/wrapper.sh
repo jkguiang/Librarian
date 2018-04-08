f2aee9441c5d4aaa2928837f76de07f510be8d49
Samuel May <sjmay@ucsd.edu>
2018-04-08 01:24:04 -0700

    Add absolute path so cron runs shell script correctly

--- nfs_backups/wrapper.sh
+++ nfs_backups/wrapper.sh
@@ -1,4 +1,6 @@
-export SCRAM_ARCH=slc6_amd64_gcc630   # or whatever scram_arch you need for your desired CMSSW release
+# Script that is actually called by cron:
+# crontab -l: 0 2 * * * /home/users/sjmay/Librarian/nfs_backups/wrapper.sh
+export SCRAM_ARCH=slc6_amd64_gcc630
 export CMSSW_VERSION=CMSSW_9_4_0
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 cd /cvmfs/cms.cern.ch/$SCRAM_ARCH/cms/cmssw/$CMSSW_VERSION/src
@@ -9,4 +11,4 @@ if [ ! -d logs/ ]; then
   mkdir logs
 fi
 
-python backup.py
+python /home/users/sjmay/Librarian/nfs_backups/backup.py
